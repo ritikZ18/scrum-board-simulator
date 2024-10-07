6e1dcbaed4d0ba2480fd8d27b3f455204e53bfad
bchinava@asu.edu <bchinava@asu.edu>
2024-10-06 21:03:30 -0700

    Create a sprint duration attribute on the new simulation page

--- src/main/java/com/groupesan/project/java/scrumsimulator/mainpackage/ui/dialogs/simulation/GeneralPage.java
+++ src/main/java/com/groupesan/project/java/scrumsimulator/mainpackage/ui/dialogs/simulation/GeneralPage.java
@@ -10,59 +10,71 @@ import java.awt.*;
 import javax.swing.*;
 
 class GeneralPage extends Wizard.WizardPage {
-    private final DataModel<String> simulationModel;
-    private final DataModel<Object> sprintModel;
+        private final DataModel<String> simulationModel;
+        private final DataModel<Object> sprintModel;
+        private final DataModel<Object> sprintDurationModel;
 
-    public GeneralPage(DataModel<String> simulationModel, DataModel<Object> sprintModel) {
-        this.simulationModel = simulationModel;
-        this.sprintModel = sprintModel;
-    }
+        public GeneralPage(DataModel<String> simulationModel, DataModel<Object> sprintModel,
+                        DataModel<Object> sprintDurationModel) {
+                this.simulationModel = simulationModel;
+                this.sprintModel = sprintModel;
+                this.sprintDurationModel = sprintDurationModel;
+        }
 
-    @Override
-    protected String getId() {
-        return "General";
-    }
+        @Override
+        protected String getId() {
+                return "General";
+        }
 
-    @Override
-    protected JPanel render() {
-        JPanel container = new JPanel(new BorderLayout());
-        ResuableHeader resuableHeader =
-                new ResuableHeader("General", "General simulation settings");
+        @Override
+        protected JPanel render() {
+                JPanel container = new JPanel(new BorderLayout());
+                ResuableHeader resuableHeader = new ResuableHeader("General", "General simulation settings");
 
-        JPanel inputs = new JPanel(new GridBagLayout());
-        TextInput simulationInput =
-                new TextInput(
-                        "Name: ", new JTextField(simulationModel.getData(), 5), simulationModel);
-        SpinnerInput sprintInput =
-                new SpinnerInput(
-                        "Sprints: ",
-                        new JSpinner(new SpinnerNumberModel(1, 1, 20, 1)),
-                        sprintModel);
+                JPanel inputs = new JPanel(new GridBagLayout());
+                TextInput simulationInput = new TextInput(
+                                "Name: ", new JTextField(simulationModel.getData(), 5), simulationModel);
+                SpinnerInput sprintInput = new SpinnerInput(
+                                "Sprints: ",
+                                new JSpinner(new SpinnerNumberModel(1, 1, 20, 1)),
+                                sprintModel);
+                SpinnerInput sprintDurationInput = new SpinnerInput(
+                                "Sprint Duration: ",
+                                new JSpinner(new SpinnerNumberModel(1, 1, 20, 1)),
+                                sprintDurationModel);
 
-        inputs.add(
-                resuableHeader,
-                new GridBagConstraintsBuilder()
-                        .setGridX(0)
-                        .setGridY(0)
-                        .setWeightX(1)
-                        .setFill(GridBagConstraints.HORIZONTAL)
-                        .setInsets(new Insets(0, 0, 5, 0)));
-        inputs.add(
-                simulationInput,
-                new GridBagConstraintsBuilder()
-                        .setGridX(0)
-                        .setGridY(1)
-                        .setWeightX(1)
-                        .setFill(GridBagConstraints.HORIZONTAL));
-        inputs.add(
-                sprintInput,
-                new GridBagConstraintsBuilder()
-                        .setGridX(0)
-                        .setGridY(2)
-                        .setWeightX(1)
-                        .setFill(GridBagConstraints.HORIZONTAL));
+                inputs.add(
+                                resuableHeader,
+                                new GridBagConstraintsBuilder()
+                                                .setGridX(0)
+                                                .setGridY(0)
+                                                .setWeightX(1)
+                                                .setFill(GridBagConstraints.HORIZONTAL)
+                                                .setInsets(new Insets(0, 0, 5, 0)));
+                inputs.add(
+                                simulationInput,
+                                new GridBagConstraintsBuilder()
+                                                .setGridX(0)
+                                                .setGridY(1)
+                                                .setWeightX(1)
+                                                .setFill(GridBagConstraints.HORIZONTAL));
+                inputs.add(
+                                sprintInput,
+                                new GridBagConstraintsBuilder()
+                                                .setGridX(0)
+                                                .setGridY(2)
+                                                .setWeightX(1)
+                                                .setFill(GridBagConstraints.HORIZONTAL));
 
-        container.add(inputs, BorderLayout.NORTH);
-        return container;
-    }
+                inputs.add(
+                                sprintDurationInput,
+                                new GridBagConstraintsBuilder()
+                                                .setGridX(0)
+                                                .setGridY(3)
+                                                .setWeightX(1)
+                                                .setFill(GridBagConstraints.HORIZONTAL));
+
+                container.add(inputs, BorderLayout.NORTH);
+                return container;
+        }
 }
