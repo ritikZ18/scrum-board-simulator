c573fa44d37202e4f1eef48665beb7b0529d72e4
Piyush Sharma <pshar169@asu.edu>
2024-10-11 22:01:07 -0700

    push us 19 changes to dev

---
+++ src/test/java/com/groupesan/project/java/scrumsimulator/mainpackage/impl/SprintTest.java
@@ -0,0 +1,29 @@
+package com.groupesan.project.java.scrumsimulator.mainpackage.impl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class SprintTest {
+    private Sprint mySprint;
+
+    @BeforeEach
+    public void setup() {
+        mySprint = SprintFactory.getSprintFactory().createNewSprint("testSprint", "testSprint", 5);
+    }
+
+    @Test
+    public void testDecrementDay() {
+        int duration = mySprint.getDaysRemaining();
+        mySprint.decrementRemainingDays();
+        assertEquals(duration - 1, mySprint.getDaysRemaining());
+    }
+
+    @Test
+    public void testAddUserStory(){
+        UserStory testUserStory = new UserStory("testUserStory", 1.0, 5.0);
+        mySprint.addUserStory(testUserStory);
+        assertEquals(true, mySprint.getUserStories().contains(testUserStory));
+    }
+}
